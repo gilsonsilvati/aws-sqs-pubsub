330fc63e193523f0c90497854e814fd1a1086d12
Gilson Silva <gilson.silva.ext@panonline.com.br>
2024-02-02 16:55:39 -0300

    first commit

---
+++ src/main/java/br/com/sqs/AwsSqsPubsubApplication.java
@@ -0,0 +1,13 @@
+package br.com.sqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSqsPubsubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSqsPubsubApplication.class, args);
+	}
+
+}
